3e8d7fb4f5442b4a9ae51e8f0f97790b0134a22a
Garrett <Garrettdeblois13@yahoo.com>
2019-01-18 19:59:09 -0600

    Refractoring some old code of mine. Replaced chain of else-if statements with a switch case statement. Added generic error handling. Disabled maximizing and resizing of form.

--- GasPriceCalculator/Form1.cs
+++ GasPriceCalculator/Form1.cs
@@ -1,73 +1,69 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace GasPriceCalculator
 {
     public partial class Form1 : Form
-    {//test
+    {
         public Form1()
         {
             InitializeComponent();
         }
         private void Form1_Load(object sender, EventArgs e)
         {
-            comboBox1.Items.AddRange(new string[] { "1/8 Tank", "1/4 Tank", "3/8 Tank", "1/2 Tank", "5/8 Tank", "3/4 Tank", "7/8 Tank" });
+            comboBoxGasLevel.Items.AddRange(new string[] { "1/8 Tank", "1/4 Tank", "3/8 Tank", "1/2 Tank", "5/8 Tank", "3/4 Tank", "7/8 Tank" });
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (comboBox1.SelectedItem == null)
+            if (comboBoxGasLevel.SelectedItem == null)
             {
                 MessageBox.Show("Please Select your Current Gas Level", "Error");
             }
-            else if (textBox1.Text == "" || textBox2.Text == "")
+            else if (textBoxTankCapacity.Text == "" || textBoxGasPrice.Text == "")
             {
                 MessageBox.Show("Please Enter your Tank Size & the Current Gas Price", "Error");
             }
             else
             {
-                double gallons = Convert.ToDouble(textBox1.Text);
-                double price = Convert.ToDouble(textBox2.Text);
-                double gasGauge = 0;
-
-                if (comboBox1.SelectedItem == "1/8 Tank")
+                double gallons = 0, price = 0, gasGauge = 0, total = 0;
+                try
                 {
-                    gasGauge = 0.875;
+                    gallons = Convert.ToDouble(textBoxTankCapacity.Text);
+                    price = Convert.ToDouble(textBoxGasPrice.Text);
                 }
-                else if (comboBox1.SelectedItem == "1/4 Tank")
+                catch
                 {
-                    gasGauge = 0.75;
-                }
-                else if (comboBox1.SelectedItem == "3/8 Tank")
-                {
-                    gasGauge = 0.625;
-                }
-                else if (comboBox1.SelectedItem == "1/2 Tank")
-                {
-                    gasGauge = 0.5;
-                }
-                else if (comboBox1.SelectedItem == "5/8 Tank")
-                {
-                    gasGauge = 0.375;
-                }
-                else if (comboBox1.SelectedItem == "3/4 Tank")
-                {
-                    gasGauge = 0.25;
-                }
-                else if (comboBox1.SelectedItem == "7/8 Tank")
-                {
-                    gasGauge = 0.125;
+                    MessageBox.Show("Please enter a number.");
                 }
 
-                double total = gallons * price * gasGauge;
-                label6.Text = total.ToString("F2"); //F2 rounds up and also makes it so that if the result has only a tens decimal and no ones decimal, it will add a trailing zero to the tens decimal (ex: .9 becomes .90 to make it look more like money)
+                switch (comboBoxGasLevel.SelectedItem)
+                {
+                    case "1/8 Tank":
+                        gasGauge = 0.875;
+                        break;
+                    case "1/4 Tank":
+                        gasGauge = 0.75;
+                        break;
+                    case "3/8 Tank":
+                        gasGauge = 0.625;
+                        break;
+                    case "1/2 Tank":
+                        gasGauge = 0.5;
+                        break;
+                    case "5/8 Tank":
+                        gasGauge = 0.375;
+                        break;
+                    case "3/4 Tank":
+                        gasGauge = 0.25;
+                        break;
+                    case "7/8 Tank":
+                        gasGauge = 0.125;
+                        break;
+                }
+
+                total = gallons * price * gasGauge;
+                labelTotalCost.Text = total.ToString("F2"); //F2 rounds up and also makes it so that if the result has only a tens decimal and no ones decimal, it will add a trailing zero to the tens decimal (ex: .9 becomes .90 to make it look more like money)
             }
         }
     }
